d466fcd1f1ffae5f8f0172b1557c6a98cbe9c8d0
multiverse007 <68814446+multiverse007@users.noreply.github.com>
2026-04-22 22:44:44 +0530

    Add files via upload

---
+++ April22nd/SumOfArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Initializing the variables
+            Console.Write("Enter the array length : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] a = new int[n];
+            int sum = 0;
+            Console.WriteLine("Enter the Array elements");
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for(int i = 0; i < n; i++)
+            {
+                sum += a[i];
+            }
+            Console.WriteLine("sum of the array is " + sum);
+        }
+    }
+}
---
+++ April22nd/Transpose/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Initializing the variables
+            Console.Write("Enter rows : ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter columns : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] a = new int[m, n];
+
+            Console.WriteLine("Enter the Matrix 1 elements");
+            for(int i= 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    a[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            int[,] aT = new int[n, m];
+            for(int i= 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    aT[j, i] = a[i, j];
+                }
+            }
+            Console.WriteLine("The Original Matrix is : ");
+            for(int i= 0; i < m; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("The Transpose of the Matrix is : ");
+            for(int i= 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {
+                    Console.Write(aT[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
